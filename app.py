65fbf4cb6aac102a28b752fbefe995c68cb9b44c
oksana <opashkovska85@gmail.com>
2022-03-18 11:21:25 +0100

    add location suggestion, contact form

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from cgi import print_environ
+from unicodedata import name
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -7,98 +8,27 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
+app.secret_key = b'\xd2%\\\xb6\x89g\xee\r\xce\x1f\x8b\x10u\xd1|\xa0'
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure CS50 Library to use SQLite database
-# db = SQL("sqlite:///cityguide.db")
-
-PLACES = [{
-    'id': 1,
-    'name': 'Museum of Illisuons',
-    'url': 'https://hamburg.museumderillusionen.de/en/',
-    'imgUrl': 'https://i.ibb.co/grRBM2d/icon-illus.png',
-    'phone': '040 30707105',
-    'img1': 'https://i.ibb.co/dbR6txJ/illus1.jpg',
-    'img2': 'https://i.ibb.co/zHqygg3/IMG-4617.jpg',
-    'img3': 'https://i.ibb.co/kx80Jv3/illus3.jpg',
-    'img4': 'https://i.ibb.co/pR8WnS6/illus4.jpg',
-    'text':
-    'Enter the fascinating world of illusions! We will trick your senses and amaze you while doing it. Visit us and you learn that nothing is what it seems, especially not in the Museum of Illusions!',
-    'lat': 53.55292,
-    'lng': 10.0021,
-    'address': 'Lilienstraße 14-16, 20095 Hamburg',
-}, {
-    'id': 2,
-    'name': 'Hamburger Puppentheater',
-    'url': 'https://hamburgerpuppentheater.de',
-    'text':
-    'The Hamburg Puppet Theatre is located under the roof of Haus Flachsland and offers a varied theatre programme for young and old.',
-    'lat': 53.583240,
-    'lng': 10.043950,
-    'address': 'Bramfelder Straße 9, 22305 Hamburg',
-}, {
-    'id': 3,
-    'name': 'Zoological Museum',
-    'url':
-    'https://hamburg.leibniz-lib.de/en/ausstellungen/museum-zoologie.html',
-    'text': '',
-    'lat': 53.567315,
-    'lng': 9.978716,
-    'address': '',
-}, {
-    'id': 4,
-    'name': 'Indoor Playground Rabatzz',
-    'url': 'https://www.rabatzz.de/',
-    'text': '',
-    'lat': 53.599832,
-    'lng': 9.915805,
-}, {
-    'id': 5,
-    'name': 'Emigration Museum',
-    'url': 'https://www.ballinstadt.de/en/',
-    'text': '',
-    'lat': 53.520396,
-    'lng': 10.016970,
-    'address': '',
-}, {
-    'id': 6,
-    'name': 'The Chocolate Museum ',
-    'url': 'https://www.chocoversum.de/',
-    'text': '',
-    'lat': 53.547935,
-    'lng': 10.002276,
-    'address': '',
-}, {
-    'id': 7,
-    'name': 'Miniature Wonderland',
-    'url': 'https://www.miniatur-wunderland.com/',
-    'text': '',
-    'lat': 53.543729,
-    'lng': 9.988516,
-    'address': '',
-}, {
-    'id': 8,
-    'name': 'The Puppenmuseum Falkenstein',
-    'url': 'https://www.elke-droescher.de/',
-    'text': '',
-    'lat': 53.56599,
-    'lng': 9.763900000000035,
-    'address': '',
-}]
+db = SQL("sqlite:///cityguide.db")
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', places=PLACES)
+    places = db.execute("SELECT * FROM places WHERE active = 'yes'")
+    return render_template('index.html', places=places)
 
 
 @app.route('/places/<id>')
 def places(id):
-    for place in PLACES:
-        if place['id'] == int(id):
-            print(place['name'])
+    places = db.execute("SELECT * FROM places")
+    for place in places:
+        print(place["active"])
+        if int(place['id']) == int(id):
             name = place["name"]
             address = place["address"]
             text = place["text"]
@@ -108,9 +38,7 @@ def places(id):
             img4 = place["img4"]
             url = place["url"]
             phone = place["phone"]
-            print(img1)
 
-    # return f'Welcome to place'
     return render_template('place.html',
                            name=name,
                            address=address,
@@ -128,9 +56,60 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/contact.html')
+@app.route('/contact.html', methods=["GET", "POST"])
 def contact():
-    return render_template('contact.html')
+    message = ''
+    if request.method == "POST":
+        name = request.form.get("name")
+        lastName = request.form.get("lastName")
+        email = request.form.get("email")
+        text = request.form.get("message")
+        print(name)
+        print(text)
+        if not name:
+            message = "You must provide your name"
+            return render_template('contact.html', message=message)
+        elif not lastName:
+            message = "You must provide your last name"
+            return render_template('contact.html', message=message)
+        elif not email:
+            message = "You must provide your email"
+            return render_template('contact.html', message=message)
+
+        db.execute(
+            "INSERT INTO contact (first_name, last_name, email, message) VALUES (?, ?, ?, ?)",
+            name, lastName, email, text)
+
+        flash("Your message was sent!")
+        return redirect("contact.html")
+    else:
+        return render_template('contact.html')
+
+
+@app.route('/add.html', methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        name = request.form.get("name")
+        address = request.form.get("address")
+        phone = request.form.get("phone")
+        web = request.form.get("web")
+        text = request.form.get("text")
+        photo1 = request.form.get("photo1")
+        photo2 = request.form.get("photo2")
+        photo3 = request.form.get("photo3")
+        photo4 = request.form.get("photo4")
+        print(photo1)
+
+        db.execute(
+            "INSERT INTO places (name, phone, address, url, img1, img2, img3, img4, text, active) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ? )",
+            name, phone, address, web, photo1, photo2, photo3, photo4, text,
+            "no")
+
+        flash("Your submission was successful!")
+        return redirect("/")
+
+    else:
+        return render_template('add.html')
 
 
 # if __name__ == ‘__main__’:
